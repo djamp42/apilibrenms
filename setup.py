72a86401b33b0391e489004e5c1777b061ff0995
Adam Piasecki <apiasecki@gmail.com>
2020-12-01 09:22:47 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apilibrenms',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/djamp42/apilibrenms',
+    license='',
+    author='Adam Piasecki',
+    author_email='apiasecki@gmail.com',
+    description='First shot at Github Python Module'
+)
