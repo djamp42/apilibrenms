2b55e861b81a0c453fbc7b868db4fd0da40956df
Adam <apiasecki@gmail.com>
2023-01-05 15:15:13 -0500

    Fix device_del call

--- apilibrenms/apilibrenms.py
+++ apilibrenms/apilibrenms.py
@@ -120,8 +120,8 @@ class LibrenmsApi:
         r = requests.post(api_url, json=add_request, headers=self.request_headers)
         return r.text
     def device_del(self, del_request):
-        api_url = f"{self.api_url}devices"
-        r = requests.delete(api_url, json=del_request, headers=self.request_headers)
+        api_url = f"{self.api_url}devices/{del_request}"
+        r = requests.delete(api_url, headers=self.request_headers)
         return r.text
 
     def device_update(self, hostname, update_request):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='apilibrenms',
-    version='23.1.1',
+    version='23.1.5',
     packages=['apilibrenms'],
     url='https://github.com/djamp42/apilibrenms',
     license='',
